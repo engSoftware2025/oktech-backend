9f1a1416f6da8b5b7501fcf7c3b496562c691cab
Lucas Carneiro do Ouro <lusca0320@gmail.com>
2025-08-07 22:05:49 -0300

    feat(shop): create endpoint to list all shops, and get all products by shopId

--- src/main/java/com/oktech/boasaude/controller/ShopController.java
+++ src/main/java/com/oktech/boasaude/controller/ShopController.java
@@ -9,10 +9,13 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.oktech.boasaude.dto.ProductResponseDto;
 import com.oktech.boasaude.dto.ShopCreateRequestDto;
 import com.oktech.boasaude.dto.ShopResponseDto;
+import com.oktech.boasaude.entity.Product;
 import com.oktech.boasaude.entity.User;
-import com.oktech.boasaude.service.ShopService;
+import com.oktech.boasaude.service.ShopService; 
+import com.oktech.boasaude.service.ProductService;
 
 
 import org.springframework.security.core.Authentication;
@@ -23,8 +26,10 @@ import jakarta.validation.Valid;
 import java.util.Map;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 
 /**
  * ShopController é responsável por gerenciar as operações relacionadas às lojas.
@@ -32,6 +37,8 @@ import org.slf4j.LoggerFactory;
  * criar novas lojas, atualizar informações de lojas existentes, etc.
  * @author João Martins
  * @version 1.0
+ * @author Lucas do Ouro
+ * @version 1.1 - Adicionado método para listar todas as lojas com paginação.
  */
 
 @RestController
@@ -39,14 +46,46 @@ import org.slf4j.LoggerFactory;
 
 public class ShopController {
     private final ShopService shopService;
+    private final ProductService productService;
 
 
     private static final Logger logger = LoggerFactory.getLogger(ShopController.class);
 
-    public ShopController(ShopService shopService) {
+    public ShopController(ShopService shopService, ProductService productService) {
         this.shopService = shopService;
+        this.productService = productService;
     }
 
+    /**
+     * Lista todas as lojas com paginação.
+     * @param pageable Objeto Pageable para paginação.
+     * @return ResponseEntity com a lista de lojas paginadas.
+     */
+    @GetMapping("/all")
+    public ResponseEntity<Page<ShopResponseDto>> ListgetAllShops(Pageable pageable) {
+        
+        Page<ShopResponseDto> shops = shopService.getAllShops(pageable);
+        return ResponseEntity.ok(shops);
+    }
+
+    @GetMapping("/{shopId}/products")
+    public ResponseEntity<Page<ProductResponseDto>> getProductsByShopId(@PathVariable UUID shopId, Pageable pageable) {
+        
+        logger.info("Fetching products for shop ID: {}", shopId);
+
+        Page<Product> productsPage = productService.getProductsByShopId(shopId, pageable);
+
+        Page<ProductResponseDto> responseDtoPage = productsPage.map(ProductResponseDto::new);
+
+        return ResponseEntity.ok(responseDtoPage);
+    }
+
+    /**
+     * Cria uma nova loja associada ao usuário autenticado.
+     * @param shopCreateDto DTO com os dados da loja a ser criada.
+     * @param authentication Objeto Authentication do Spring Security.
+     * @return ResponseEntity com a loja criada.
+     */
     @PostMapping("/create")
     public ResponseEntity<ShopResponseDto> createShop(@Valid @RequestBody ShopCreateRequestDto shopCreateDto, Authentication authentication) {
         try
@@ -82,6 +121,12 @@ public class ShopController {
         return ResponseEntity.ok(shopResponse);     
     }
 
+    /**
+     * Deleta uma loja associada ao usuário autenticado.
+     * @param id ID da loja a ser deletada.
+     * @param authentication Objeto Authentication do Spring Security.
+     * @return ResponseEntity com a mensagem de sucesso ou erro.
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Map<String, String>> deleteShop(@PathVariable UUID id, Authentication authentication){
         if (authentication == null || !(authentication.getPrincipal() instanceof User)) {
@@ -98,6 +143,13 @@ public class ShopController {
         return ResponseEntity.ok(Map.of("message", "Shop deleted"));
     }
 
+    /**
+     * Atualiza uma loja associada ao usuário autenticado.
+     * @param id ID da loja a ser atualizada.      
+     * @param shopCreateDto DTO com os dados atualizados da loja.
+     * @param authentication Objeto Authentication do Spring Security.
+     * @return ResponseEntity com a loja atualizada.
+     */
     @PutMapping("/{id}")
     public ResponseEntity<ShopResponseDto> updateShop(@PathVariable UUID id, @Valid @RequestBody ShopCreateRequestDto shopCreateDto, Authentication authentication){
         if (authentication == null || !(authentication.getPrincipal() instanceof User)) {
